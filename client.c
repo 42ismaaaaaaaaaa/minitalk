e8e8593e683f686ff34672599c7ac0cd189c8e23
Ismael OUALI <iouali@e3r4p6.42.fr>
2021-06-15 14:47:21 +0200

    first final

---
+++ client.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iouali <iouali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/14 13:47:32 by iouali            #+#    #+#             */
+/*   Updated: 2021/06/15 14:43:47 by iouali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+int		ft_atoi(const char *nptr);
+void	send_signal(pid_t pid, char c);
+char	*char_to_bin(char c);
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+	int		y;
+	char	*bin;
+
+	if (argc != 3)
+		return (1);
+	i = 0;
+	while (argv[2][i])
+	{
+		bin = char_to_bin(argv[2][i]);
+		y = 0;
+		while (bin[y])
+		{
+			send_signal(ft_atoi(argv[1]), bin[y]);
+			y++;
+		}
+		i++;
+	}
+	free(bin);
+	return (0);
+}
+
+void 	send_signal(pid_t pid, char c)
+{
+	if (c == '0')
+		kill(pid, SIGUSR1);
+	if (c == '1')
+		kill(pid, SIGUSR2);
+	usleep(50);
+}
+
+char	*char_to_bin(char c)
+{
+	char	*bin;
+	int		i;
+	int		div;
+	int		mod;
+
+	i = 7;
+	bin = malloc(9 * sizeof(char));
+	if (!bin)
+		return (NULL);
+	div = c;
+	while (i >= 0)
+	{
+		mod = div % 2;
+		if (mod == 0)
+			bin[i] = '0';
+		else
+			bin[i] = '1';
+		div /= 2;
+		i--;
+	}
+	bin[8] = '\0';
+	return (bin);
+}
+
+static int	is_space(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\v' || c == '\r' || c == '\f'
+		|| c == '\n')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	size_t			i;
+	unsigned char	*tmp_nptr;
+	int				sign;
+	long long		result;
+
+	tmp_nptr = (unsigned char *)nptr;
+	sign = 1;
+	result = 0;
+	i = 0;
+	while (is_space(tmp_nptr[i]))
+		i++;
+	if (tmp_nptr[i] == '-' || tmp_nptr[i] == '+')
+	{
+		if (tmp_nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (tmp_nptr[i] >= '0' && tmp_nptr[i] <= '9')
+	{
+		result = (result * 10) + (tmp_nptr[i] - 48);
+		i++;
+	}
+	return (result * sign);
+}
---
+++ server.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void	sighandler(int signum);
+void	bin_to_char(char *bin);
+int		ft_pow(int power, int nb);
+void	ft_putint(int pid);
+
+int	main(void)
+{
+	int	pid;
+
+	pid = getpid();
+	if (!pid)
+		return (1);
+	ft_putint(pid);
+	write(1, "\n", 1);
+	while (1)
+	{
+		if (signal(SIGUSR1, sighandler) == SIG_ERR)
+			return (1);
+		if (signal(SIGUSR2, sighandler) == SIG_ERR)
+			return (1);
+	}
+	return (0);
+}
+
+void	sighandler(int signum)
+{
+	static char	bin[8];
+	int			i;
+
+	i = 0;
+	while (bin[i])
+		i++;
+	if (signum == SIGUSR1)
+		bin[i] = '0';
+	else if (signum == SIGUSR2)
+		bin[i] = '1';
+	bin[i + 1] = '\0';
+	if (i == 7)
+	{
+		bin_to_char(bin);
+		bin[0] = '\0';
+	}
+}
+
+int	ft_pow(int power, int nb)
+{
+	int	result;
+
+	result = 1;
+	while (power > 0)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
+
+void	bin_to_char(char *bin)
+{
+	int	i;
+	int	y;
+	int	num;
+	int	zero_one;
+
+	num = 0;
+	i = 7;
+	y = 0;
+	while (i >= 0)
+	{
+		zero_one = 0;
+		if (bin[y] == '1')
+			zero_one = 1;
+		num += (zero_one * ft_pow(i, 2));
+		i--;
+		y++;
+	}
+	write(1, &num, 1);
+}
+
+void	ft_putint(int pid)
+{
+	int		mod;
+	int		to_print;
+
+	mod = pid % 10;
+	pid /= 10;
+	to_print = mod + 48;
+	if (pid > 0)
+	{
+		ft_putint(pid);
+		write(1, &to_print, 1);
+	}
+	if (pid == 0)
+		write(1, &to_print, 1);
+}
